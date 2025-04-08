b6a73ffbbe7df7658b0033aeb5003c7fa2664fb0
YunJuHyung <wngud5289@naver.com>
2025-04-09 00:28:08 +0900

    feat CategoryView
    - button custom delegate pattern

--- PilloskProject/PilloskProject/Models/DataService.swift
+++ PilloskProject/PilloskProject/Models/DataService.swift
@@ -7,23 +7,22 @@
 import Foundation
 
 class DataService {
-    
+
     enum DataError: Error {
         case fileNotFound
         case parsingFailed
     }
-    func loadData(completion: @escaping (Result<[ProductData], Error>) -> Void) {
-        guard let path = Bundle.main.url(forResource: "Resources/data", withExtension: "json") else {
+    func loadData(completion: @escaping (Result<ProductData, Error>) -> Void) {
+        guard let path = Bundle.main.path(forResource: "data", ofType: "json") else {
             completion(.failure(DataError.fileNotFound))
             return
         }
-            
+
             do {
-                let data = try Data(contentsOf: URL(filePath: "Resources/data"))
-                let ProductData = try JSONDecoder().decode(ProductData.self, from: data)
-                completion(.success([ProductData]))
-            }
-            catch {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path))
+                let productData = try JSONDecoder().decode(ProductData.self, from: data)
+                completion(.success(productData))
+            } catch {
                 print("data passingFailed")
                 completion(.failure(DataError.parsingFailed))
             }
