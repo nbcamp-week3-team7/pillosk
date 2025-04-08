e1715cbe3d314c9c94151d7946c4f3a1c78cd530
ksyq12 <ksyq12@daum.net>
2025-04-08 10:40:27 +0900

    add: Utils/Constants.swift 추가

---
+++ PilloskProject/PilloskProject/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  PilloskProject
+//
+//  Created by shinyoungkim on 4/8/25.
+//
+
+import Foundation
+
+enum CellIdentifier {
+    static let productCell = "ProductCell"
+}
