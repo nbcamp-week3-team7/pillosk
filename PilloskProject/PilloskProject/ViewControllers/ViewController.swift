8d49564caaf118d1c61da03030c2ccf4799d8d7a
LEEBUYONG <dqd001@naver.com>
2025-04-08 13:19:52 +0900

    fix: 테이블뷰 오토 레이아웃 제약 설정

--- PilloskProject/PilloskProject/ViewControllers/ViewController.swift
+++ PilloskProject/PilloskProject/ViewControllers/ViewController.swift
@@ -9,15 +9,25 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    let orderSummaryVC = OrderSummaryView()
     
     let categoryView = CategoryView()
-    let orderSummaryView = OrderSummaryView()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addOrderSummaryViewController()
     }
-
-
+    
+    func addOrderSummaryViewController() {
+        addChild(orderSummaryVC)
+        view.addSubview(orderSummaryVC.view)
+        orderSummaryVC.view.frame = view.bounds
+        orderSummaryVC.didMove(toParent: self)
+        
+        orderSummaryVC.view.snp.makeConstraints { make in
+            make.height.equalTo(400)
+            make.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(10)
+        }
+    }
 }
 
--- PilloskProject/PilloskProject/Views/OrderSummaryView.swift
+++ PilloskProject/PilloskProject/Views/OrderSummaryView.swift
@@ -7,6 +7,62 @@
 import UIKit
 import SnapKit
 
+class OrderSummaryView: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let orderTableView = UITableView()
+    let summayCountLabel = UILabel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        orderTableView.backgroundColor = .yellow
+        
+        setTableView()
+        summaryCountLabel()
+    }
+    
+    // ===== 테이블뷰 세팅 =====
+    func setTableView() {
+        orderTableView.frame = view.bounds
+        orderTableView.dataSource = self
+        orderTableView.delegate = self
+        
+        [orderTableView, summayCountLabel].forEach { view.addSubview($0) }
+        
+        orderTableView.register(StackTableViewCell.self, forCellReuseIdentifier: "stackCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "stackCell", for: indexPath) as! StackTableViewCell
+        
+        // 두 개의 레이블에만 텍스트 설정
+        cell.nameLabel.text = "Row \(indexPath.row) - NameLabel"
+        cell.priceLabel.text = "Row \(indexPath.row) - PriceLabel"
+            
+            return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func summaryCountLabel() {
+        summayCountLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        summayCountLabel.text = ("총 3개 입니다.")
+        summayCountLabel.textAlignment = .left
+        
+        summayCountLabel.snp.makeConstraints { make in
+            make.height.equalTo(30)
+            make.width.equalTo(70)
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview().offset(10)
+        }
+    }
+}
+
 // ===== 스택뷰 테이블셀 커스텀 정의 =====
 class StackTableViewCell: UITableViewCell {
     let stackView = UIStackView()
@@ -20,56 +76,13 @@ class StackTableViewCell: UITableViewCell {
         stackView.spacing = 5
         stackView.alignment = .leading
         stackView.distribution = .fillEqually
-        
-        nameLabel.text = "TestNameLabel"
-        priceLabel.text = "TestPriceLabel"
+        stackView.backgroundColor = .blue
         
         [nameLabel, priceLabel].forEach() { stackView.addArrangedSubview($0) }
         contentView.addSubview(stackView)
-        
-        stackView.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(10)
-            make.top.bottom.trailing.equalToSuperview().inset(10)
-        }
-        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder): has not been implemented")
-    }
-}
-
-class OrderSummaryView: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    let orderTableView = UITableView()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        orderTableView.backgroundColor = .yellow
-        
-        orderTableView.frame = view.bounds
-        orderTableView.dataSource = self
-        orderTableView.delegate = self
-        
-        orderTableView.register(StackTableViewCell.self, forCellReuseIdentifier: "stackCell")
-        view.addSubview(orderTableView)
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "stackCell", for: indexPath) as! StackTableViewCell
-        
-        // 두 개의 레이블에만 텍스트 설정
-        cell.nameLabel.text = "Row \(indexPath.row) - 첫 번째 텍스트"
-        cell.priceLabel.text = "Row \(indexPath.row) - 두 번째 텍스트"
-            
-            return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
     }
 }
