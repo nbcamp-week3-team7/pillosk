59f10428a0c769b605b550855484a1cfd1fe9597
YunJuHyung <wngud5289@naver.com>
2025-04-09 00:32:42 +0900

    feat ViewController
    - load Json.data
    - add categoryView use func configureUI()

--- PilloskProject/PilloskProject/ViewControllers/ViewController.swift
+++ PilloskProject/PilloskProject/ViewControllers/ViewController.swift
@@ -8,15 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     let categoryView = CategoryView()
+    private let dataService = DataService()
+    private var productData: [ProductData] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(categoryView)
+        categoryView.delegate = self
+        loadData()
+        configureUI()
+
     }
 
+    func loadData() {
+        dataService.loadData { [weak self] result in
+            guard let self = self else { return }
 
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let productData):
+                    self.productData = [productData]
+                    let categoryNames = productData.categories.map { $0.name }
+                    self.categoryView.notifyCategoryButtonsUpdate(categories: categoryNames)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+    }
+
+    func configureUI() {
+
+        categoryView.backgroundColor = .brown
+
+        view.addSubview(categoryView)
+        categoryView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(120)
+//            $0.leading.trailing.equalToSuperview()
+        }
+    }
 }
 
+extension ViewController: CategoryViewDelegate {
+    func setCategoryButtons(categories: [String]) {
+        categoryView.notifyCategoryButtonsUpdate(categories: categories)
+    }
+}
